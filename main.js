5d052c4df042f7daf0d658dfdfd9cc6e91dbcb53
Stephen Settle <ssettle3@gmail.com>
2015-01-13 15:20:17 -0500

    Homework JS-Intro Questions Completed

---
+++ main.js
@@ -0,0 +1,56 @@
+var name;
+// Q: What does name equal right now?
+// A: nothing
+
+name = "Tim";
+// Q: What is `name` set to right now?
+// A: Tim
+
+name = "Bill";
+// Q: What is `name` set to right now?
+// A: Bill
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+// A: Hello, how are you?
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// A: 40
+
+math = high - "5";
+// Q: What is `math` set to?
+// A: 45
+
+// Create a variable to calculate my age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+var age = "Tim is " +(today - born) + " years old."
+
+
+
+
+// Store some information following in variables.
+var yourname;
+var myname;
+var statement = yourname + " is taking a class at The Iron Yard, and my instructor is " + myname;
+
+
+//Answer 
+var yourname;
+undefined
+yourname = 'Stephen'
+"Stephen"
+var myname;
+undefined
+myname = 'Tim'
+"Tim"
+var statement = yourname + " is taking a class at The Iron Yard, and my instructor is " + myname;
+undefined
+statement = yourname + " is taking a class at The Iron Yard, and my instructor is " + myname;
+"Stephen is taking a class at The Iron Yard, and my instructor is Tim"
